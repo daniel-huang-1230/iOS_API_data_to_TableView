bb127c955dc502a13a3f431031349271b7a20ca6
Daniel Huang <gn03249822@DanieltekiMacBook-Pro.local>
2017-09-07 19:37:44 -0700

    Class created and function implemented

---
+++ Country.swift
@@ -0,0 +1,25 @@
+//
+//  Country.swift
+//  Country&Capital
+//
+//  Created by Daniel Huang on 9/7/17.
+//  Copyright © 2017 Daniel Huang. All rights reserved.
+//
+
+import Foundation
+
+class Country{
+    
+    var country : String
+    var capital : String
+    
+    
+    //initializer
+    
+    init(country: String, capital: String){
+        
+        self.country = country
+        self.capital = capital
+    }
+    
+};
--- ViewController.swift
+++ ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //an array of Country objects
+    var fetchedCountries = [Country]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        parseData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //the function to manage the url request and parse the data from the url
+    func parseData(){
+        
+        fetchedCountries = [] //an empty array initially
+        let url = "https://restcountries.eu/rest/v1/all"
+        
+        var request = URLRequest(url: URL(string: url)!)
+        
+        request.httpMethod = "GET"  //use the default "GET" method
+        
+        let configuration = URLSessionConfiguration.default
+        
+        let session = URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main )
+        
+        //Creates a task that retrieves the contents of the specified URL.
+        let task = session.dataTask(with: request, completionHandler: { (data, response, error)in
+            if error != nil{
+                print("Error")
+            } else {
+                do {
+                     let fetchedData = try JSONSerialization.jsonObject(with: data!, options: .mutableLeaves) as! NSArray
+                    
+                    //print(fetchedData) //for debugging purpose, print it to the console
+                    for eachFetchedCountry in fetchedData{
+                        
+                        let eachCountry = eachFetchedCountry as! [String:Any]
+                        
+                        let country = eachCountry["name"] as! String
+                        let capital = eachCountry["capital"] as! String
+                        self.fetchedCountries.append(Country(country: country, capital: capital))
+                    }
+                    
+                    print(self.fetchedCountries) //for debugging purpose
+                }
+                catch{
+                    
+                    print("Error2")
+                }
+                
+            }
+            
+        })
+        
+        
+        task.resume()
+        
+    }
 }
 
